6cd70c122298bb4486312f8bebc7de78b66367a2
Krista Rutz <rutzkri000@gmail.com>
2020-03-17 16:52:43 -0700

    Create links to sort by breed, type, date, or name
    Co-authored-by: Adela Darmansyah <adela.yohana@gmail.com>

--- AnimalShelter.Solution/AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter.Solution/AnimalShelter/Controllers/AnimalsController.cs
@@ -16,9 +16,29 @@ namespace AnimalShelter.Controllers
       _db = db;
     }
 
-    public ActionResult Index()
+    public ActionResult Index(string sortBy)
     {
-      List<Animal> model = _db.Animals.ToList();
+      List<Animal> sortedBy = _db.Animals.ToList();
+
+      if (sortBy == "byName")
+      {
+        sortedBy = _db.Animals.OrderBy(animals => animals.Name).ToList();
+      }
+      else if (sortBy == "byBreed")
+      {
+        sortedBy = _db.Animals.OrderBy(animals => animals.Breed).ToList();
+      }
+      else if (sortBy == "byType")
+      {
+        sortedBy = _db.Animals.OrderBy(animals => animals.AnimalType).ToList();
+      }
+      else if (sortBy == "byDateAdmitted")
+      {
+        sortedBy = _db.Animals.OrderBy(animals => animals.DateAdmitted).ToList();
+      }
+
+      Dictionary<string, List<Animal>> model = new Dictionary<string, List<Animal>> {};
+      model.Add("sortBy", sortedBy);
       return View(model);
     }
 
